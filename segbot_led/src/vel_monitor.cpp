a5d337bf7b0b08c31c21691126a443a27c166512
patel1.shivam2@gmail.com <patel1.shivam2@gmail.com>
2017-05-06 17:44:02 -0500

    weekend

--- segbot_led/src/vel_monitor.cpp
+++ segbot_led/src/vel_monitor.cpp
@@ -30,21 +30,37 @@
 *                 Global Variables                     *
 ********************************************************/
 ros::Publisher signal_pub;
-
+ros::Subscriber global_path;  
 ros::Subscriber robot_vel;
-
 geometry_msgs::Twist vel_msg;
-
 bool heard_vel = false;
+bool heard_path = false;
+bool heard_pose = false;
+nav_msgs::Path current_path;
+geometry_msgs::Pose current_pose;
+ros::Subscriber robot_pose;  
 
 /*******************************************************
 *                 Callback Functions                   *
 ********************************************************/
-// Updates the current path
+// Updates the current velocity
 void vel_cb(const geometry_msgs::Twist::ConstPtr& msg)
 {
    vel_msg = *msg;
    heard_vel = true;
+}
+
+// Updates the current path
+void path_cb(const nav_msgs::Path::ConstPtr& msg) {
+  current_path = *msg; 
+  heard_path = true; 
+}
+
+// Updates the current pose
+void pose_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
+    geometry_msgs::PoseWithCovarianceStamped new_pose = *msg; 
+    current_pose = new_pose.pose.pose; 
+    heard_pose = true; 
 }
 
 int main(int argc, char **argv)
@@ -59,50 +75,78 @@ int main(int argc, char **argv)
     // Sets up subscribers
     robot_vel = n.subscribe("/cmd_vel", 1, vel_cb);
 
+
+
+    // Sets up subscriber for turning
+    global_path = n.subscribe("/move_base/EBandPlannerROS/global_plan", 1, path_cb);
+    robot_pose = n.subscribe("/amcl_pose", 1, pose_cb);
+
     // Sets up action client
     actionlib::SimpleActionClient<bwi_msgs::LEDControlAction> ac("led_control_server", true);
     ac.waitForServer();
-
+    ROS_INFO("MADE AGAIN");
     bwi_msgs::LEDControlGoal goal;
+
+
 
     // Waits for current path and pose to update
     while(!heard_vel) 
     {
         ros::spinOnce();
-        //ros::Duration(7).sleep();		//jivko said we need to sleep
+        ros::Duration(7).sleep();		//jivko said we need to sleep
     }
 
-        ROS_INFO("MADE IT AGAIN");
+
+
 
 	while(ros::ok()) {
 		// Updates current path and pose
 		ros::spinOnce();
 		int vel = vel_msg.linear.x;
 		ROS_INFO("%d\n", vel);
+
+        double current_yaw = tf::getYaw(current_pose.orientation); 
+
         if(vel_msg.linear.x > 0)
         {
             //add turn functionality in here???
             ROS_INFO("POSITIVE SPEED");
             goal.type.led_animations = bwi_msgs::LEDAnimations::FORWARD;
-            goal.type.params.push_back(0.5);
+
+            //CODE FOR TURN FUNCTIONALITY FROM PREVIOUS FRI PROJECT
+            // We only traverse the first quarter of the Global Plan Array
+            for(int i = 0; i < current_path.poses.size() / 2; i++) {
+              double yaw = tf::getYaw(current_path.poses[i].pose.orientation); 
+              if(abs(current_yaw - yaw) > 0.2) {
+                  // Right turn 
+                  if(current_yaw - yaw < 0) {
+                    ROS_INFO("RIGHT TURN");
+                    goal.type.led_animations = bwi_msgs::LEDAnimations::RIGHT;    
+                  }
+                  // Left turn
+                  else {
+                    ROS_INFO("LEFT TURN");
+                    goal.type.led_animations = bwi_msgs::LEDAnimations::LEFT;
+                  } 
+              }
+            }
         }
         else if(vel_msg.linear.x < 0)
         {
             //add turn functionality in here???
 			ROS_INFO("NEGATIVE SPEED");
-			goal.type.led_animations = bwi_msgs::LEDAnimations::DOWN;
+			goal.type.led_animations = bwi_msgs::LEDAnimations::BACKWARD;
 		}
 		else
 		{
 			ROS_INFO("STOPPED");
 			goal.type.led_animations = bwi_msgs::LEDAnimations::BLOCKED;
-			//goal.timeout = ros::Duration(7);
-			//ac.sendGoal(goal);
 		}
         goal.timeout = ros::Duration(7);        
 		ac.sendGoal(goal);
 		ac.waitForResult(ros::Duration(1, 0));
 		loop_rate.sleep();        
 	}
+
     return 0;
 }
